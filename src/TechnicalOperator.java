8018539f8c782b3ff3dcb79f3e3657ab6abb7191
ruakhdt <Darya.KHMELKOVA@raiffeisen.ru>
2024-09-25 22:23:13 +0300

    fix: review #1

--- src/TechnicalOperator.java
+++ src/TechnicalOperator.java
@@ -1,51 +1,61 @@
 public class TechnicalOperator {
 
     // Задание 1. https://tproger.ru/articles/zadachi-po-java-dlya-nachinayushhih
+    // Напишите программу, которая выводит на экран числа от 1 до 10.
     public void printNumbersInOrder() {
         int countUp = 1;
-
-        while (countUp <= 10) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(countUp);
             countUp++;
         }
     }
 
     // Задание 2. https://tproger.ru/articles/zadachi-po-java-dlya-nachinayushhih
+    // Напишите программу, которая находит сумму всех чисел от 1 до 100.
     public void printSumOfNumbers() {
         int number = 1;
         int result = 0;
         while (number <= 100) {
-            result = result + number;
+            result += number;
             number++;
             System.out.println(result);
         }
     }
 
     // Задание 3. https://tproger.ru/articles/zadachi-po-java-dlya-nachinayushhih
+    // Найдите факториал заданного числа.
     public void printFactorialFunctionResult(int factorialNumber) {
         if (factorialNumber == 0) {
             System.out.println(0);
         } else {
             int result = 1;
             for (int i = 1; i <= factorialNumber; i++) {
-                result = result * i;
+                result *= i;
                 System.out.println(result);
             }
         }
     }
 
     // Задание 4. https://tproger.ru/articles/zadachi-po-java-dlya-nachinayushhih
+    // Напишите проверку, является ли заданное число простым.
     public void isSimpleNumber(int numberForCheck) {
         int some = 2;
-        while (some < numberForCheck) {
-            int result = numberForCheck % some;
-            if (result == 0) {
-                System.out.println("Число не является простым");
-                break;
-            }
-            some++;
-            if (some == numberForCheck - 1) {
-                System.out.println("Число является простым");
+        if (numberForCheck == 1) {
+            System.out.println("В математике принято не относить единицу ни к простым, ни к составным числам");
+        } else if (numberForCheck == 2) {
+            System.out.println("Это первое наименьшее простое, единственное четное, простое число");
+        } else {
+            while (some < numberForCheck) {
+                int result = numberForCheck % some;
+                if (result == 0) {
+                    System.out.println("Число не является простым");
+                    break;
+                }
+                some++;
+                if ((some == numberForCheck - 1) || (some == numberForCheck)) {
+                    System.out.println("Число является простым");
+                    break;
+                }
             }
         }
     }
@@ -54,7 +64,7 @@ public class TechnicalOperator {
     // Дано целое число. Выведите в консоль первую цифру этого числа.
     public void getFirstNumber(int numberForEdit) {
         String number = String.valueOf(numberForEdit);
-        String firstNumber = number.substring(0, 1);
+        char firstNumber = number.charAt(0);
         System.out.println("Если взять число " + numberForEdit + ", то его первая цифра будет " + firstNumber);
     }
 
@@ -92,13 +102,11 @@ public class TechnicalOperator {
     // Дана строка. Если в этой строке более одного символа, выведите в консоль предпоследний символ этой строки.
     public void getPenultimateSymbol(String combinationOfSymbols) {
         int numberLength = combinationOfSymbols.length();
-        if (numberLength == 1){
+        if (numberLength == 1) {
             System.out.println("В строке " + combinationOfSymbols + " только 1 символ");
-        }
-        else if (numberLength == 0){
+        } else if (numberLength == 0) {
             System.out.println("Вы передали пустую строку");
-        }
-        else {
+        } else {
             String penultimateSymbol = combinationOfSymbols.substring(numberLength - 2, numberLength - 1);
             System.out.println("В строке " + combinationOfSymbols + " предпоследний символ: " + penultimateSymbol);
         }
@@ -108,19 +116,15 @@ public class TechnicalOperator {
     // Дано целое число, содержащее номер минуты от 0 до 60: Определите, в какую четверть часа попадает это значение.
     public void getQuarterOfAnHour(byte num) {
 
-        if  ((0 <= num) && (num <= 15)) {
+        if ((0 <= num) && (num <= 15)) {
             System.out.println("Попадает в первую четверть часа");
-        }
-        else if ((15 < num) && (num <= 30)) {
+        } else if ((15 < num) && (num <= 30)) {
             System.out.println("Попадает во вторую четверть часа");
-        }
-        else if ((30 < num) && (num <= 45)) {
+        } else if ((30 < num) && (num <= 45)) {
             System.out.println("Попадает в третью четверть часа");
-        }
-        else if ((45 < num) && (num <= 60)) {
+        } else if ((45 < num) && (num <= 60)) {
             System.out.println("Попадает в четвертую четверть часа");
-        }
-        else {
+        } else {
             System.out.println("Значение за границами интервала от 0 до 60, поэтому не может быть соотнесено с четвертью часа");
         }
     }
